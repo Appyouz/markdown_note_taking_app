df6d8cf4305316d1180635bc6fb2dcbdd6db8f5a
appyouz <dikpalkhatri7@gmail.com>
2025-08-11 15:55:21 +0545

    refactor: file_upload to upload_file

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,32 @@
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.views import Response
 from .models import Notes
 from .serializers import NoteSerializer
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser
 
 class NoteViewSet(ModelViewSet):
     queryset = Notes.objects.all()
     serializer_class = NoteSerializer
+    parser_classes = [MultiPartParser] # Add this line
+
+    @action(detail=False, methods=['post'])
+    def upload_file(self,request):
+        uploaded_file = request.FILES['markdown_file']
+
+        # Read the file content
+        file_content = uploaded_file.read()
+
+        # Decode content which is in bytes to a string
+        decoded_content = file_content.decode('utf-8')
+
+        # Save the notes
+        note_instance = Notes.objects.create(title=uploaded_file.name, content=decoded_content)
+        serializer = NoteSerializer(note_instance)
+        
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+
+
+
