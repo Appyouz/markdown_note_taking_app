4a8b12d2782f625a6886816a15ec9371806ab38d
appyouz <dikpalkhatri7@gmail.com>
2025-08-11 15:17:34 +0545

    feat: add defaultrouters

--- notes/urls.py
+++ notes/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import NoteViewSet
 
+# create a router instance
+router = DefaultRouter()
+
+# Register the viewset with the router
+# where the first argument 'notes' is the URL prefix for this viewset
+router.register(r'notes', NoteViewSet, basename='notes')
+
+app_name = 'notes'
 urlpatterns = [
+    path('', include(router.urls)),
 ]
